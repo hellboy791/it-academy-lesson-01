2a4e7536c3fec0337ac8fca0e23c567c0ade8c84
Alexandr Stepanenko <79439521+hellboy791@users.noreply.github.com>
2024-10-01 20:14:11 +0300

    Щупаем github

---
+++ src/by/epam/tr/main/HomeTask.java
@@ -0,0 +1,7 @@
+package by.epam.tr.main;
+
+public class HomeTask{
+	public static void main(String[] args) {
+		System.out.println("A\nB\nC\nD\nE\n");
+	}
+}
---
+++ src/edu/training/jc/simple_output/TextOutput.java
@@ -0,0 +1,8 @@
+package edu.training.jc.simple_output;
+
+class TextOutput{
+	public static void main(String[] args) {
+		System.out.println("\t Long, long ago there lived a young\n and very beautiful girl called Cinderella.");
+		System.out.println("\t Cinderella lived with her stepmother\n and stepsisters. The sisters were wicked and\n haughty.");
+	}
+}
