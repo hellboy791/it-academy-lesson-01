6ea275353f8a7742cabc0689b746b03cee25cd97
Alexandr Stepanenko <79439521+hellboy791@users.noreply.github.com>
2024-10-01 20:38:51 +0300

    Update TextOutput.java

--- src/edu/training/jc/simple_output/TextOutput.java
+++ src/edu/training/jc/simple_output/TextOutput.java
@@ -1,6 +1,6 @@
 package edu.training.jc.simple_output;
 
-class TextOutput{
+public class TextOutput{
 	public static void main(String[] args) {
 		System.out.println("\t Long, long ago there lived a young\n and very beautiful girl called Cinderella.");
 		System.out.println("\t Cinderella lived with her stepmother\n and stepsisters. The sisters were wicked and\n haughty.");
