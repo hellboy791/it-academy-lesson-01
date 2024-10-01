051a3f7261bfcbde30c0fec50e34c8161c691d92
Alexandr Stepanenko <79439521+hellboy791@users.noreply.github.com>
2024-10-01 20:38:29 +0300

    Update StairsOutput.java

--- src/edu/training/jc/simple_output/StairsOutput.java
+++ src/edu/training/jc/simple_output/StairsOutput.java
@@ -1,11 +1,7 @@
 package edu.training.jc.simple_output;
 
-class StairsOutput{
+public class StairsOutput{
 	public static void main(String[] args) {
-		//System.out.println(" \"To wisely live your life, you don't need to know much\n\n\t Just remember two main rules for the beginning:\n\n\t\t You better starve, than eat whatever\n\n\t And better be alone, than with whoever.\”");
-		//System.out.println(" \"To wisely live your life, you don't need to know much Just remember two main rules for the beginning: You better starve, than eat whatever And better be alone, than with whoever.\" Omar Khayyam, Rubaiyat”);
 		System.out.println(" \"To wisely live your life, you don't need to know much\n\n\t Just remember two main rules for the beginning:\n\n\t\t You better starve, than eat whatever\n\n\t And better be alone, than with whoever.\"\n\n Omar Khayyam, Rubaiyat");
-
-	
 	}
 }
